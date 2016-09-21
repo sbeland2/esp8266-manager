b7ca8d83d1083b7a511babe310aced5f6cb2d675
Serge Beland <sbeland@gmail.com>
2016-09-21 18:47:36 +0700

    Add files via upload

---
+++ php/serverinfo.php
@@ -0,0 +1,13 @@
+<?php
+
+$MaxNbOfEmailServer = 2;
+$ServerEmail = array ("sbelandiot@gmail.com", "sbelandiot2@gmail.com");
+$ServerEmailPw = "type your email password for all email address"; // set to all email address the same pw or make an array that match ServerEmail array
+$ServerFromName = "Serge Beland";
+
+$IotPath = "./iot/";
+
+$IotSessionPath = $IotPath.$email.".session";	
+$IotEmailPath = $IotPath.$email.".txt";
+
+?>
